8e8b065ea78c5f0afcd58bdb98dcaa58ceff8198
Nipat <That.guy.nipat@gmail.com>
2020-05-09 23:57:34 +0700

    Update

--- FaceDetection and Cropping/FaceDetectPic.py
+++ FaceDetection and Cropping/FaceDetectPic.py
@@ -5,6 +5,7 @@ Created on Mon Apr 27 09:34:59 2020
 @author: nipat
 """
 from imutils.face_utils import FaceAligner
+import openface.openface.align_dlib as openface
 import dlib
 import numpy as np
 import cv2
@@ -19,11 +20,13 @@ net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
 alignFile = "shape_predictor_68_face_landmarks.dat"
 predictor = dlib.shape_predictor(alignFile)
 fa = FaceAligner(predictor, desiredFaceWidth=128)
+face_aligner = openface.AlignDlib(alignFile)
 
 # read image
-image = cv2.imread("rooster.jpg")
+
+image = cv2.imread("./test/test3.jpg")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-print(gray)
+
 # check for existing images
 path, dirs, files = next(os.walk("./images/"))
 file_count = len(files)
@@ -48,19 +51,22 @@ for i in range(0, detections.shape[2]):
     # find box boundary
     box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
     (startX, startY, endX, endY) = box.astype("int")
- 
+    #startX = left; startY = top; endX = right; endy = bottom;
     # draw box around face & probability
     text = "{:.2f}%".format(confidence * 100)
     y = startY - 10 if startY - 10 > 10 else startY + 10
-    cv2.rectangle(image, (startX, startY), (endX, endY),(0, 0, 255), 2)
-    cv2.putText(image, text, (startX, y),
-    cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+    #cv2.rectangle(image, (startX, startY), (endX, endY),(0, 0, 255), 2)
+    #cv2.putText(image, text, (startX, y),
+    #cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+    dlibBox = dlib.rectangle(left = startX,top = startY,right = endX,bottom = endY)
+    aligned_face = fa.align(image, gray, dlibBox)
+    alignedFace = face_aligner.align(534, image, dlibBox, landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
 
 img_name = "images/opencv_frame_{}.jpeg".format(file_count)
-cv2.imwrite(img_name, image[startY:endY, startX:endX])
+cv2.imwrite(img_name, alignedFace) #image[startY:endY, startX:endX]
 print("{} written!".format(img_name))
 
-cv2.imshow("Frame", image)
+cv2.imshow("Frame", aligned_face)
     
 cv2.destroyAllWindows()
 # vs.stream.stream.release()
