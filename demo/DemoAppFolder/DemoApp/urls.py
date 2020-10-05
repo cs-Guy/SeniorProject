0c157656f7ca4db63090376fb8bd97c5fbed4e6d
Nipat <nipat.phattarakijtham@mail.kmutt.ac.th>
2020-10-05 17:22:19 +0700

    asdf

--- demo/DemoAppFolder/DemoApp/urls.py
+++ demo/DemoAppFolder/DemoApp/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from prediction import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('^$',views.index,name='homepage'),
     path('api/', include('prediction.urls')),
 ]
